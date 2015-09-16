24a4dd78e13e09ab13bef950220da5b1f7abf376
Josh Moore <joshsmoore@gmail.com>
2015-09-16 14:00:51 +0800

    Log Reader is working
    * When created start at end of log
    * Once a log message is read it can never be reread
    * If a log file is rotated start reading from beginning of file

--- lib/log_parser_notifier/log_reader.rb
+++ lib/log_parser_notifier/log_reader.rb
@@ -1,7 +1,20 @@
 module LogParserNotifier
   class LogReader
-    def initialize
-      # LogParserNotifier.logger.debug("creating logger")
+    def initialize(log_file)
+      LogParserNotifier.logger.debug("creating log reader for: '#{log_file}'")
+      @log_file = File.open(log_file, 'rb')
+      @log_file.seek(@log_file.size, IO::SEEK_SET)
+      LogParserNotifier.logger.debug("staring position at: #{@log_file.pos}")
+    end
+
+    def log_io
+      @log_file.seek(0, IO::SEEK_SET) if @log_file.pos > @log_file.size
+
+      @log_file
+    end
+
+    def read_to_end
+      log_io.read
     end
   end
 end
--- spec/log_reader_spec.rb
+++ spec/log_reader_spec.rb
@@ -2,8 +2,35 @@ require 'spec_helper'
 
 module LogParserNotifier
   describe LogReader do
-    it 'should fail' do
-      expect(true).to eq(false)
+    let :log_reader do
+      LogReader.new(File.join(Dir.pwd, 'spec', 'support', 'rails.log'))
+    end
+
+    after do
+      FileUtils.cp 'spec/support/rails.log.back', 'spec/support/rails.log'
+    end
+
+    it 'when created it should start reading from the end of the file' do
+      expect(log_reader.read_to_end).to eq('')
+    end
+
+    it 'should read the new lines if log file is updated after reading' do
+      log_reader # force initialize
+      FileUtils.cp 'spec/support/rails_with_new_requests.log', 'spec/support/rails.log'
+      expect(log_reader.read_to_end.split("\n").count).to eq 41
+    end
+
+    it 'should never reread the same lines' do
+      log_reader # force initialize
+      FileUtils.cp 'spec/support/rails_with_new_requests.log', 'spec/support/rails.log'
+      log_reader.read_to_end
+      expect(log_reader.read_to_end).to eq ''
+    end
+
+    it 'should start from the beginning if the log file is rotated' do
+      log_reader # force initialize
+      FileUtils.cp 'spec/support/rails_rotated.log', 'spec/support/rails.log'
+      expect(log_reader.read_to_end.split("\n").count).to eq 19
     end
   end
 end
